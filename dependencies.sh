eca97ad03f520e63e12a97b3c4c05403538c4cf9
Zach Garwood <zachgarwood@gmail.com>
2018-11-16 19:33:23 -0600

    Made dependencies installable on macOS

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,11 @@
 install () {
     echo "Installing dependencies..."
-    brew tap msoap/tools
-    brew install shell2http
+    if [ "$(uname -s)" == "Darwin" ]; then
+        brew tap msoap/tools
+        brew install shell2http
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	snap install shell2http
+    fi
     echo "Installed."
 }
 
